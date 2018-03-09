489c978488b245205f596cc716fea7d150b1ab5e
maikgibran <maik.gibran.rantetasik@kreasitindo.co.id>
2018-03-09 17:53:28 +0700

    Implement item stacking for the same item

---
+++ Assets/Scripts/Items/InventoryItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+using RogueGem.Player;
+
+namespace RogueGem.Items{
+	public class InventoryItem{
+
+		private int amount;
+		private string name;
+
+		public InventoryItem (Item item){
+			amount = item.GetAmount ();
+			name = item.GetName ();
+		}
+
+		public void UseOn(PlayerBehaviour player){
+			player.Heal (amount);
+		}
+
+		public void AddAmount(int addByAmount){
+			amount = amount + addByAmount;
+		}
+
+		public string GetName(){
+			return name;
+		}
+
+		public int GetAmount(){
+			return amount;
+		}
+	}
+}
+
