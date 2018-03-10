061ad1a08d7ad6cb7e3c1268e7cd8682f134022a
Unknown <feladorn@gmail.com>
2018-03-10 18:30:21 -0500

    Basic minimap functionality, add grids to tiles

---
+++ Assets/Scripts/Utilities/MinimapBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RogueGem.Utilities;
+
+public class MinimapBehavior : MonoBehaviour {
+	private Board board;
+
+	void Start () {
+		board = Board.Instance;
+		EventBehaviour.StartListening(GameEvent.MoveEnemy, revealTiles);
+	}
+
+	public void revealTiles() {
+		GameObject[] walls = GameObject.FindGameObjectsWithTag ("Obstacle");
+		foreach (GameObject wall in walls) {
+			if (WorldController.IsTileInSight(wall.transform.position)) {
+				Debug.Log ("In sight: " + wall.transform.Find("Minimap").transform.position);
+				wall.transform.Find("Minimap").transform.GetComponent<SpriteRenderer> ().enabled = true;
+			}
+
+		}
+
+	}
+
+
+}
